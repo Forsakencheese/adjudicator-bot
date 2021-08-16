e5d6d2dd4acc6f1b4262d0c7e04ab266b26bd5b7
Ben Morgan <morganbenjamin968@gmail.com>
2021-08-16 14:13:43 -0500

    added more wisdom, implemented a simple for loop for poll function

--- adjudbot.py
+++ adjudbot.py
@@ -16,9 +16,9 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-bot = commands.Bot(command_prefix='*')#establishing connection with discord to adjudicator
+bot = commands.Bot(command_prefix='^')#establishing connection with discord to adjudicator
 
-@bot.command(name='bestow', help= 'give a small chunk of wisdom')
+@bot.command(name='bestow', help= '---give a small chunk of wisdom')
 async def on_message(ctx): #Blurt random wisdom on_message is
 
     wisdom_quotes = [
@@ -30,22 +30,42 @@ async def on_message(ctx): #Blurt random wisdom on_message is
         'Fear denies Faith',
         'DAMN THEE TO HELL',
         (
-            'There is no enemy. The foe on the battlefield is merely the manifestation of that which we must overcome.'
-            'He is doubt, and fear, and despair. Every battle is fought within. Conquer the battlefield that lies inside'
+            'There is no enemy. The foe on the battlefield is merely the manifestation of that which we must overcome. '
+            'He is doubt, and fear, and despair. Every battle is fought within. Conquer the battlefield that lies inside '
             'you, and the enemy disappears like the illusion he is'
         ),
         'Remember my dear brothers, the honed blade cuts deepest',
-        'Hope is the first step on the road to disappointment'
+        'Hope is the first step on the road to disappointment',
+        'Knowledge is power, Hide it well',
+        'Through discipline, we prevail',
+        'MIND OVER MATTER',
+        'Faith is purest when it is unquestioning',
+        'Bless the mind too small for doubt',
+        'A Wise man gets more from his enemies than a fool from his friends'
     ]
 
 
     response = random.choice(wisdom_quotes)
     await ctx.send(response)
 
-@bot.command(name='poll', help=' Creates a poll for the judging of the guilty')
-async def poll_poll(ctx):
+@bot.command(name='poll', help='---Creates a poll for the judging of the guilty, Syntax: ^poll "question" "option 1" "option 2"...."option n < 10"')
+async def simple_poll(ctx, question, *options: str): #*options: str, holding this back just in case question doesn't work
+    print(question)
+    print(options)
+    reactions = ['dart']
 
-    response = 'Be silent, I must learn this knowledge first'
-    await ctx.send(response)
+    counter = 1
+    for x in options:
+        await ctx.send(f'Option {counter}: {x}')
+        counter = counter + 1
+
+
+#    await ctx.send(question)
+#     else:
+#       response = 'Be silent, I am still acquiring this knowledge'
+#        with open('knowledge.jpg', 'rb') as f:
+#            picture = discord.File(f)
+#            await ctx.channel.send(file=picture)
+#        await ctx.send(response)
 
 bot.run(TOKEN)
